1756190e2c24b0ffc39713e8710232c633fb2856
mithuleshgutha <mithulesh.gvn234@gmail.com>
2023-06-20 22:40:54 +0530

    .

---
+++ src/components/Work/WorkCardData.jsx
@@ -0,0 +1,57 @@
+import hcms from './../../assets/hcms2.png'
+import banking from './../../assets/banking.jpg'
+import bca from './../../assets/bca.png'
+import fos from './../../assets/fos.png'
+import netflixclone from './../../assets/netflixclone.png'
+import splitwise from './../../assets/splitwise.jpeg'
+import sas from './../../assets/sas.png'
+import rpwc from './../../assets/rpwc.png'
+import calc from './../../assets/calc.png'
+import cp from './../../assets/cp.jpeg'
+import api from './../../assets/api.jpg'
+import agecalc from './../../assets/agecalc.jpg'
+
+const projectCardData = [
+    
+    {
+        imgsrc: bca,
+        title: "Basic Calculator App",
+        text: "A basic calculator app in which we can do basic calculations like addition , multiplication , subtraction and division",
+        viewurl: "https://ravitejachowdarytask2.netlify.app/",
+        sourceurl: "https://github.com/RavitejaChowdhary/reactjs_task2.git"
+    },
+    
+    {
+        imgsrc: splitwise,
+        title: "SplitWise",
+        text: "This is a website which is was developed using html , css , javascript and php in which it is used to split the bill among friends or colleagues when they went for party.",
+        viewurl: "https://github.com/RavitejaChowdhary/SplitWise.git",
+        sourceurl: "https://github.com/RavitejaChowdhary/SplitWise.git"
+    },
+    
+    {
+        imgsrc: rpwc,
+        title: "Paragraph Word Counter",
+        text: "This a paragraph word counter where when a user enters a paragraph it counts and displays number of words in the given paragraph.",
+        viewurl: "https://ravitejachowdarytask3.netlify.app/",
+        sourceurl: "https://github.com/RavitejaChowdhary/reactjs_task1.git"
+    },
+    {
+        imgsrc: cp,
+        title: "Color Picker",
+        text: "This is a color picker website where when a user select a color it will change the button color to the selected color.",
+        viewurl: "https://ravitejachowdarytask1.netlify.app/",
+        sourceurl: "https://github.com/RavitejaChowdhary/reactjs_task3.git"
+    },
+    {
+        imgsrc: api,
+        title: "Fetch data from API",
+        text: "This is the project where we used axios to fetch data from the API's and displaying those details in the client side.",
+        viewurl: "https://ravitejachowdhary.github.io/reactjs_task4/",
+        sourceurl: "https://ravitejachowdhary.github.io/reactjs_task4/"
+    },
+    
+
+]
+
+export default projectCardData
