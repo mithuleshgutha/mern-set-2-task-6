4a583a8c741b96509044509ba9201be588951eb9
mithuleshgutha <mithulesh.gvn234@gmail.com>
2023-06-20 23:06:02 +0530

    .

--- src/components/Work/WorkCardData.jsx
+++ src/components/Work/WorkCardData.jsx
@@ -17,41 +17,25 @@ const projectCardData = [
         imgsrc: bca,
         title: "Basic Calculator App",
         text: "A basic calculator app in which we can do basic calculations like addition , multiplication , subtraction and division",
-        viewurl: "https://ravitejachowdarytask2.netlify.app/",
-        sourceurl: "https://github.com/RavitejaChowdhary/reactjs_task2.git"
-    },
-    
-    {
-        imgsrc: splitwise,
-        title: "SplitWise",
-        text: "This is a website which is was developed using html , css , javascript and php in which it is used to split the bill among friends or colleagues when they went for party.",
-        viewurl: "https://github.com/RavitejaChowdhary/SplitWise.git",
-        sourceurl: "https://github.com/RavitejaChowdhary/SplitWise.git"
+        viewurl: "https://mithuleshchowdary-task-2.netlify.app/",
+        sourceurl: "https://github.com/mithuleshgutha/MERN-STACK-SET-2/tree/main/task-2"
     },
     
     {
         imgsrc: rpwc,
         title: "Paragraph Word Counter",
         text: "This a paragraph word counter where when a user enters a paragraph it counts and displays number of words in the given paragraph.",
-        viewurl: "https://ravitejachowdarytask3.netlify.app/",
-        sourceurl: "https://github.com/RavitejaChowdhary/reactjs_task1.git"
+        viewurl: "https://mithuleschowdary-task-1.netlify.app/",
+        sourceurl: "https://github.com/mithuleshgutha/MERN-STACK-SET-2/tree/main/task-1"
     },
     {
         imgsrc: cp,
         title: "Color Picker",
         text: "This is a color picker website where when a user select a color it will change the button color to the selected color.",
-        viewurl: "https://ravitejachowdarytask1.netlify.app/",
-        sourceurl: "https://github.com/RavitejaChowdhary/reactjs_task3.git"
-    },
-    {
-        imgsrc: api,
-        title: "Fetch data from API",
-        text: "This is the project where we used axios to fetch data from the API's and displaying those details in the client side.",
-        viewurl: "https://ravitejachowdhary.github.io/reactjs_task4/",
-        sourceurl: "https://ravitejachowdhary.github.io/reactjs_task4/"
+        viewurl: "https://mithuleshchowdary-task-3.netlify.app/",
+        sourceurl: "https://github.com/mithuleshgutha/MERN-STACK-SET-2/tree/main/task-3"
     },
     
-
 ]
 
 export default projectCardData
