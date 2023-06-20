37aea8f23525089ea8ea2e65df5c8e556da89b92
mithuleshgutha <mithulesh.gvn234@gmail.com>
2023-06-20 22:52:57 +0530

    .

--- src/components/AboutContent/AboutContent.jsx
+++ src/components/AboutContent/AboutContent.jsx
@@ -9,7 +9,7 @@ const AboutContent = () => {
         <div className='about'>
             <div className="left">
                 <h1>Who Am I</h1>
-                <p>Hello 👋, my name is Raviteja Neppali. I am currently pursuing B.Tech Fourth year in Computer Science and Engineering at VIT Chennai. I am a front end developer with great passion for website design. I have been working on the front end for some time, have also created some projects which you can see on my GitHub and will continue to do so. Apart from front end skills, I also know some programming languages like c, c++, python and java. A lot of work has also been done on machine learning and some projects have also been created. But right now my focus is on the front end because these skills give me the freedom to make anything new and innovative so that I can find a solution of any problem.</p>
+                <p>Hello, I am mithulesh chowdary, I am currently pursuing my B.Tech in Computer Science and Engineering at VIT chennai</p>
                 <Link to="/contact">
                     <button className="btn">Contact</button>
                 </Link>
